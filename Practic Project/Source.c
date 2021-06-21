e1917bc4934ea60bc4b5cd5a9d8f8c15bb3719dc
zkoma <zkomarnitskiy@gmail.com>
2021-06-21 13:14:41 +0300

    Добавления коментариев

--- Practic Project/Source.c
+++ Practic Project/Source.c
@@ -16,8 +16,8 @@
 
 int main()
 {
-    int number_day[SizeMonth][1] = { {31},{29},{31},{30},{31},{30},{31},{31},{30},{31},{30},{31} };
-    setlocale(LC_ALL, "Ukr");
+    int number_day[SizeMonth][1] = { {31},{29},{31},{30},{31},{30},{31},{31},{30},{31},{30},{31} }; // Кількість днів в місяці
+    setlocale(LC_ALL, "Ukr"); // Підключення української мови
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
     int day = 0, month = 0;
@@ -30,7 +30,7 @@ int main()
         case 'ц': case 'Ц': case 87: case 119: {
             printf("Введіть день та місяць: ");
             scanf_s("%d%d", &day, &month);
-            (day <= number_day[month - 1][0] && day >= 1 && month <= 12 && month >= 1) ? print_day(day), print_month(month) : print_error();
+            (day <= number_day[month - 1][0] && day >= 1 && month <= 12 && month >= 1) ? print_day(day), print_month(month) : print_error(); // Перевірка на кількість днів в місяці так кількість місяців
             break;
         }
         case 'Й': case 'й': case 113: case 81: printf("Робота програми завершена!\n");  return 0;
